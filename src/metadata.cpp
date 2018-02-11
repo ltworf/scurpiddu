b5a779fce8bcf08005b173095018014321b9e8e1
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2018-02-11 21:04:05 +0100

    Added properties for important metadata

--- src/metadata.cpp
+++ src/metadata.cpp
@@ -26,8 +26,49 @@ Metadata::Metadata(QObject *parent) : QObject(parent)
 
 void Metadata::setMetadata(QString key, QString value) {
     _map[key] = value;
+    if (key == "album")
+        emit albumChanged(value);
+    else if (key == "album_artist")
+        emit album_artistChanged(value);
+    else if (key == "artist")
+        emit artistChanged(value);
+    else if (key == "comment")
+        emit commentChanged(value);
+    else if (key == "date")
+        emit dateChanged(value);
+    else if (key == "disc")
+        emit discChanged(value);
+    else if (key == "genre")
+        emit genreChanged(value);
+    else if (key == "publisher")
+        emit publisherChanged(value);
+    else if (key == "title")
+        emit titleChanged(value);
+    else if (key == "track")
+        emit trackChanged(value);
 }
 
 void Metadata::clear() {
     _map.clear();
+    emit albumChanged("");
+    emit album_artistChanged("");
+    emit artistChanged("");
+    emit commentChanged("");
+    emit dateChanged("");
+    emit discChanged("");
+    emit genreChanged("");
+    emit publisherChanged("");
+    emit titleChanged("");
+    emit trackChanged("");
 }
+
+QString Metadata::album() { return _map.value("album", ""); }
+QString Metadata::album_artist() { return _map.value("album_artist", ""); }
+QString Metadata::artist() { return _map.value("artist", ""); }
+QString Metadata::comment() { return _map.value("comment", ""); }
+QString Metadata::date() { return _map.value("date", ""); }
+QString Metadata::disc() { return _map.value("disc", ""); }
+QString Metadata::genre() { return _map.value("genre", ""); }
+QString Metadata::publisher() { return _map.value("publisher", ""); }
+QString Metadata::title() { return _map.value("title", ""); }
+QString Metadata::track() { return _map.value("track", ""); }
--- src/metadata.h
+++ src/metadata.h
@@ -30,11 +30,43 @@ class Metadata : public QObject
 public:
     explicit Metadata(QObject *parent = nullptr);
 
+    Q_PROPERTY(QString album READ album NOTIFY albumChanged)
+    Q_PROPERTY(QString album_artist READ album_artist NOTIFY album_artistChanged)
+    Q_PROPERTY(QString artist READ artist NOTIFY artistChanged)
+    Q_PROPERTY(QString comment READ comment NOTIFY commentChanged)
+    Q_PROPERTY(QString date READ date NOTIFY dateChanged)
+    Q_PROPERTY(QString disc READ disc NOTIFY discChanged)
+    Q_PROPERTY(QString genre READ genre NOTIFY genreChanged)
+    Q_PROPERTY(QString publisher READ publisher NOTIFY publisherChanged)
+    Q_PROPERTY(QString title READ title NOTIFY titleChanged)
+    Q_PROPERTY(QString track READ track NOTIFY trackChanged)
+
 signals:
+    void albumChanged(QString);
+    void album_artistChanged(QString);
+    void artistChanged(QString);
+    void commentChanged(QString);
+    void dateChanged(QString);
+    void discChanged(QString);
+    void genreChanged(QString);
+    void publisherChanged(QString);
+    void titleChanged(QString);
+    void trackChanged(QString);
 
 public slots:
     void clear();
     void setMetadata(QString key, QString value);
+    QString album();
+    QString album_artist();
+    QString artist();
+    QString comment();
+    QString date();
+    QString disc();
+    QString genre();
+    QString publisher();
+    QString title();
+    QString track();
+
 private:
     QMap<QString,QString> _map;
 };
